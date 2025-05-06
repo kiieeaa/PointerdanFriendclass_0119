5728f22d86aa45d868da0287705bdc3ce8b4b856
kiieeaa <kiyaadinda5@gmail.com>
2025-05-06 18:26:14 +0700

    akses modifier

--- contoh.cpp
+++ contoh.cpp
@@ -3,5 +3,20 @@ using namespace std;
 
 class PersegiPanjang
 {
+public:
+     int panjang, lebar;
+
+public:
+    void input()
+    {
+        cout << "masukan panjang : " ;
+        cin >> panjang;
+        cout << "masukan lebar : " ;
+        cin >> lebar;
+    }
+    int luasPp(int a, int b)
+    {
+        return a * b;
+    }
 
 };
